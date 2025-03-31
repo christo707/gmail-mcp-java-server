00c71ad4e9a6ce61060c197ccdd2f7be69824303
Christopher Rozario <christopherrozario7@gmail.com>
2025-03-31 13:18:51 +0530

    feat: initial commit

---
+++ src/main/java/com/christo/agentic/ai/gmail_mcp_java_server/GmailMcpJavaServerApplication.java
@@ -0,0 +1,13 @@
+package com.christo.agentic.ai.gmail_mcp_java_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailMcpJavaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmailMcpJavaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/christo/agentic/ai/gmail_mcp_java_server/GmailMcpJavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christo.agentic.ai.gmail_mcp_java_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailMcpJavaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
